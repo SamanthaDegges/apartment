dff60e26d1492e193022e306eb4d0c7d943d11e0
Jameela Huq <jameela.huq@gmail.com>
2015-09-30 14:33:31 -0700

    Three views for each function

--- www/js/app.js
+++ www/js/app.js
@@ -1,29 +1,27 @@
+// window.apartmentApp = angular.module('apartmentApp', ['ui.router']);
+
+// apartmentApp.config(function($stateProvider, $urlRouterProvider) {
+
+//   $urlRouterProvider.otherwise('/');
+
+//   $stateProvider
+//     .state('login', {
+//       url: '/',
+//       templateUrl: 'templates/tab-login.html',
+//       controller: "mainCtrl"
+//     })
+//     .state('manageApt', {
+//       url: '/manageApt',
+//       templateUrl: 'templates/tab-manageApt.html',
+//       controller: "manageAptCtrl"
+//     })
+//     .state('manageUser', {
+//       url: '/manageUser',
+//       templateUrl: 'templates/tab-manageUser.html',
+//       controller: "manageUserCtrl"
+//     })
+// });
 // Ionic Starter App
-
-window.apartmentApp = angular.module('apartmentApp', ['ui.router']);
-
-apartmentApp.config(function($stateProvider, $urlRouterProvider) {
-
-  $urlRouterProvider.otherwise('/');
-
-  $stateProvider
-    .state('login', {
-      url: '/',
-      templateUrl: 'templates/tab-login.html',
-      controller: "mainCtrl"
-    })
-    .state('manageApt', {
-      url: '/manageApt',
-      templateUrl: 'templates/tab-manageApt.html',
-      controller: "manageAptCtrl"
-    })
-    .state('manageUser', {
-      url: '/manageUser',
-      templateUrl: 'templates/tab-manageUser.html',
-      controller: "manageUserCtrl"
-    })
-});
-
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
@@ -32,76 +30,109 @@ apartmentApp.config(function($stateProvider, $urlRouterProvider) {
 // 'starter.controllers' is found in controllers.js
 angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-      cordova.plugins.Keyboard.disableScroll(true);
+  .run(function($ionicPlatform) {
+    $ionicPlatform.ready(function() {
+      console.log('vibrate:', navigator.vibrate);
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        cordova.plugins.Keyboard.disableScroll(true);
 
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: '/tab',
-    abstract: true,
-    templateUrl: 'templates/tabs.html'
+      }
+      if (window.StatusBar) {
+        // org.apache.cordova.statusbar required
+        StatusBar.styleLightContent();
+      }
+    });
   })
 
-  // Each tab has its own nav history stack:
+  .config(function($stateProvider, $urlRouterProvider) {
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-      }
-    }
-  })
+    // Ionic uses AngularUI Router which uses the concept of states
+    // Learn more here: https://github.com/angular-ui/ui-router
+    // Set up the various states which the app can be in.
+    // Each state's controller can be found in controllers.js
+    $stateProvider
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
+      // setup an abstract state for the tabs directive
+      .state('tab', {
+        url: '/tab',
+        abstract: true,
+        templateUrl: 'templates/tabs.html'
+      })
+      // Each tab has its own nav history stack:
+
+      .state('tab.dash', {
+        url: '/dash',
+        views: {
+          'tab-dash': {
+            templateUrl: 'templates/tab-dash.html',
+            controller: 'DashCtrl'
+          }
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-        }
-      }
-    })
+      })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
+      // .state('tab.chats', {
+      //     url: '/chats',
+      //     views: {
+      //       'tab-chats': {
+      //         templateUrl: 'templates/tab-chats.html',
+      //         controller: 'ChatsCtrl'
+      //       }
+      //     }
+      //   })
+      //   .state('tab.chat-detail', {
+      //     url: '/chats/:chatId',
+      //     views: {
+      //       'tab-chats': {
+      //         templateUrl: 'templates/chat-detail.html',
+      //         controller: 'ChatDetailCtrl'
+      //       }
+      //     }
+      //   })
+
+      // .state('tab.account', {
+      //   url: '/account',
+      //   views: {
+      //     'tab-account': {
+      //       templateUrl: 'templates/tab-account.html',
+      //       controller: 'AccountCtrl'
+      //     }
+      //   }
+      // })
+
+      .state('tab.login', {
+        url: '/login',
+        views: {
+          'tab-login': {
+            templateUrl: 'templates/tab-login.html',
+            controller: 'loginCtrl'
+          }
+        }
+      })
+
+      .state('tab.manageUser', {
+        url: '/manageUser',
+        views: {
+          'tab-manageUser': {
+            templateUrl: 'templates/tab-manageUser.html',
+            controller: 'manageUserCtrl'
+          }
+        }
+      })
+
+      .state('tab.manageApt', {
+        url: '/manageApt',
+        views: {
+          'tab-manageApt': {
+            templateUrl: 'templates/tab-manageApt.html',
+            controller: 'manageAptCtrl'
+          }
+        }
+      });
+
+    // if none of the above states are matched, use this as the fallback
+    $urlRouterProvider.otherwise('/tab/login');
+
   });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
