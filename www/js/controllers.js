1c110205044fdbe517d61ec1a17b968f11eab989
Samantha Degges <samanthadegges@gmail.com>
2015-09-30 17:29:56 -0700

    Created login controller with a embedded service. Added ng directives to the tab-login page.

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,14 +10,45 @@ var app = angular.module('starter.controllers', [])
     };
   })
 
-  .controller('loginCtrl', function($scope) {
+
+
+
+.service('createLogin', function($http) {
+  this.assemble = function(email, password){
+    var login = {
+      email: email,
+      password: password
+    }
+    $http.post('http://localhost:3000/login', login)
+    .then (function(data){
+      console.log(data)
+    })
+    .catch (function(err){
+      console.log(err)
+    })
+  }
+})
+
+//this controller sends login data to be posted and created in DB
+  .controller('loginCtrl', function($scope, $http, createLogin) {
+    $scope.submitLoginInfo = function(){
+        createLogin.assemble($scope.email, $scope.password);
+      }
   })
+
   .controller('manageUserCtrl', function($scope, $http) {
     $scope.users = $http.get('localhost:3000/managers/');
     $scope.createuser = function(email, pwd, manager){
       $http.post('localhost:3000/managers/', {email, pwd, manager}).
       then(console.log('success'), console.log('fail'));
     }
+
+
+
+
+
+  .controller('manageUserCtrl', function($scope) {
+
   })
   .controller('manageAptCtrl', function($scope) {
 
