b50397fc7107da14fda2dfba59e98d071b2c9b1d
berkeleybear <alicewu1992@gmail.com>
2015-09-30 19:46:50 -0700

    controllers working now

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,33 +1,35 @@
 var app = angular.module('starter.controllers', [])
 
+  .controller('DashCtrl', function($scope) {
+    // $scope.title = "title"
+  })
 
-.service('Props', function($http) {
-  this.add = (newProp) => $http.post("http://localhost:3000/managers/newProperty", newProp);
-  //props.getProps = $http.get("http://localhost:3000/managers/properties")
-})
+  .controller('AccountCtrl', function($scope) {
+    $scope.settings = {
+      enableFriends: true
+    };
+  })
 
-
-.service('createLogin', function($http) {
-  this.assemble = function(email, password){
-    var login = {
-      email: email,
-      password: password
+  .service('createLogin', function($http) {
+    this.assemble = function(email, password){
+      var login = {
+        email: email,
+        password: password
+      }
+      $http.post('http://localhost:3000/login', login)
+      .then (function(data){
+        console.log(data)
+      })
+      .catch (function(err){
+        console.log(err)
+      })
     }
-    $http.post('http://localhost:3000/login', login)
-    .then (function(data){
-      console.log(data)
-    })
-    .catch (function(err){
-      console.log(err)
-    })
-  }
-})
+  })
 
-//this controller sends login data to be posted and created in DB
   .controller('loginCtrl', function($scope, $http, createLogin) {
     $scope.submitLoginInfo = function(){
-        createLogin.assemble($scope.email, $scope.password);
-      }
+      createLogin.assemble($scope.email, $scope.password);
+    }
   })
 
   .controller('manageUserCtrl', function($scope, $http) {
@@ -36,7 +38,10 @@ var app = angular.module('starter.controllers', [])
       $http.post('localhost:3000/managers/', {email, pwd, manager}).
       then(console.log('success'), console.log('fail'));
     }
-
+  })
+  .service('Props', function($http) {
+    this.add = (newProp) => $http.post("http://localhost:3000/managers/newProperty", newProp);
+    //props.getProps = $http.get("http://localhost:3000/managers/properties")
   })
   .controller('manageAptCtrl', function($scope, Props) {
     $scope.addProperty = function(prop){
@@ -48,37 +53,14 @@ var app = angular.module('starter.controllers', [])
         .catch(function(error){
           prop = error;
         });
-        //
-        //propsService.add($scope.prop)
-        //  .then(function(res) {
-        //    console.log(res.data);
-        //    $scope.prop = {};
-        //  })
-        //  .catch(function(error){
-        //    $scope.prop = error;
-        //  })
+
       };
-
-
-  //.controller('AccountCtrl', function($scope) {
-});
-
-//
-//var propertySchema =  mongoose.Schema({
-//  manager: [{type: mongoose.Schema.ObjectId, ref: 'user'}],
-//  name: String,
-//  address: String,
-//  apartments: [{type: mongoose.Schema.ObjectId, ref: 'Apartment'}]
-//});
-
-//
-
-
-  //.factory('aptFactory', function($http) {
-  //  var apts = () => {};
-  //  apts.get = () => $http.get('https://evening-beyond-5571.herokuapp.com/apts');
-  //  apts.add = (tickerObj) => $http.post('https://evening-beyond-5571.herokuapp.com/apts', {newTicker: tickerObj});
-  //  //apts.del = (tickerSym, refresh) => $http.post('https://evening-beyond-5571.herokuapp.com/delete', {tickerSymbol: tickerSym});
-  //
-  //  return Tracked;
-  //})
+    $scope.apartments = $http.get('localhost:3000/managers/newApartment/');
+    $scope.addApt = function(apartment){
+      console.log("YAY!");
+      console.log(apartment);
+      $http.post('localhost:3000/managers/newApartment/', {apartment}).
+      then(console.log('success'), console.log('fail'));
+    };
+    })
+  ;
--- www/js/services.js
+++ www/js/services.js
@@ -2,8 +2,8 @@ angular.module('starter.services', [])
 
 .controller('manageUserCtrl', function ($scope, $http, notify) {
   $scope.users = $http.get('localhost:3000/users');
-}).
-  factory('notify', ['$window', function(win) {
+})
+.factory('notify', ['$window', function(win) {
     var msgs = [];
     return function (msg) {
       msgs.push(msg);
